1e87d7f1291b1ff4f3ba1ac1c417df488a4e23a6
sncandiani <sofiancandiani@gmail.com>
2020-08-27 07:26:12 -0500

    created first two practice exercises

---
+++ date.py
@@ -0,0 +1,6 @@
+import datetime 
+# Principle: Importing datetime allows us to determine what today's date and time are
+date = datetime.date.today()
+time = datetime.datetime.now()
+
+print(date, time)
---
+++ twinkletwinkle.py
@@ -0,0 +1,4 @@
+# Principle: \n makes a new line and \t tabs over
+
+print(f"Twinkle, twinkle, little star,\n How I wonder what you are!\n\tUp above the world so high,\n\tLike a diamond in the sky\nTwinkle, twinkle, little star,\n How I wonder what you are.")
+
